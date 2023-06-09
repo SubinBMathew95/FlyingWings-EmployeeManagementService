fe6a73229dd7cca0245881c89ed00429ae163b9a
Subin B Mathew <Libin Workstation@LibinWorkstatio.bbrouter>
2023-06-09 19:11:13 +0530

    Initial Commit

---
+++ src/main/java/com/flyingwings/employeemanagementservice/FlyingWingsEmployeeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flyingwings.employeemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingWingsEmployeeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingWingsEmployeeManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyingwings/employeemanagementservice/FlyingWingsEmployeeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyingwings.employeemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingWingsEmployeeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
